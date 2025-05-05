9b7778378f7dba1d821b9148edbadbdbd9b52d5f
ANKUR SHARMA <ankur143513@gmail.com>
2025-05-05 14:43:50 +0530

    update

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -16,7 +16,7 @@ const hundreds = 1000000
 
 // +++++++++++++ Maths +++++++++++++++++++++++++++++
 
-// console.log(Math); // jkfk
+// console.log(Math); //Prints the entire Math object (with all methods)
 // console.log(Math.abs(-4));
 // console.log(Math.round(4.6));
 // console.log(Math.ceil(4.2));
